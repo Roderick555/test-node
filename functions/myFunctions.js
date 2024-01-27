04820e164cec5e5442e971af27255d52646bdf99
Roderick <fonchamroderick555@gmail.com>
2024-01-27 15:03:12 +0100

    change file data

--- functions/myFunctions.js
+++ functions/myFunctions.js
@@ -1,10 +1,12 @@
 const { translate } = require('google-translate-api-browser');
 
-translate("Bonjour", { to: "en" })
-  .then(res => {
-    // I do not eat six days
-    console.log(res.text)
-  })
-  .catch(err => {
-    console.error(err);
-  });
+function TranslateFromFrench () {
+  translate("Bonjour", { to: "en" })
+    .then(res => {
+      // I do not eat six days
+      console.log(res.text)
+    })
+    .catch(err => {
+      console.error(err);
+    });
+} 
